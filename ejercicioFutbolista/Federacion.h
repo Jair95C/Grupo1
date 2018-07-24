31326b2a8076b9299b167f595d3e83568058679a
EduardoBarragan <barragane412@gmail.com>
2018-07-24 12:50:07 -0500

    Federacion.h

--- ejercicioFutbolista/Federacion.h
+++ ejercicioFutbolista/Federacion.h
@@ -1,12 +1,12 @@
 using namespace std;
-class Federacion{
+class Federacion: public Integrante{
 	int id;
 	string nombre;
 	
 	public:
 		Federacion(){
 		}
-		Federacion(int id, string nombre){
+		Federacion(int id, string nombre):Integrante(id,nombre,apellido,edad){
 			this->id=id;
 			this->nombre=nombre;
 		}
@@ -16,5 +16,25 @@ class Federacion{
 		void setId(int id){this->id=id;}
 		void setNombre(string nombre){this->nombre=nombre;}
 		
-	
+	void ingresar(){
+			string apellidos;
+			int edad;
+			cout<<"Ingrese los datos del Entrenador"<<endl;
+			cout<<"Id:";cin>>id;
+			cout<<"Nombre:";cin>>nombre;
+			cout<<"Apellido:";cin>>apellidos;
+			cout<<"Edad:";cin>>edad;
+			
+			this->setApellidos(apellidos);
+			this->setEdad(edad);
+
+		}
+		void imprimir(){
+			cout<<" Datos del Entrenador"<<endl;
+			cout<<"ID:"<<id<<endl;
+			cout<<"Nombre:"<<nombre<<endl;
+			cout<<"Apellido:"<<this->getApellidos()<<endl;
+			cout<<"Edad:"<<this->getEdad()<<endl;
+						
+		}
 };
